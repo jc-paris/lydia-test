268132bc82922835277974170928c8b1f435915f
Jean-Christophe Paris <jc.paris@printicapp.com>
2018-08-25 09:35:33 +0200

    Initial Commit

---
+++ lydia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lydia
+//
+//  Created by Jean-Christophe Paris on 25/08/2018.
+//  Copyright © 2018 Jean-Christophe Paris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
